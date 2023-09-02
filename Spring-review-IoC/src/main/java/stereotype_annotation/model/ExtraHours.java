d159fd9219abc005460b43e01b5b132936a80ace
Abdellatif Zouliga <zuuliga@gmail.com>
2023-09-02 13:09:08 -0400

    Review Stereotype

---
+++ Spring-review-IoC/src/main/java/stereotype_annotation/model/ExtraHours.java
@@ -0,0 +1,11 @@
+package stereotype_annotation.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExtraHours {
+
+    public int getHours(){
+        return 10;
+    }
+}
