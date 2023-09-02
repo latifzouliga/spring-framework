c8140b5aa9cc2fc2f47f4682b24eb9492d2b9dea
Abdellatif Zouliga <zuuliga@gmail.com>
2023-09-02 12:43:14 -0400

    Review Stereotype

--- Spring-review-IoC/src/main/java/stereotype_annotation/MyApp.java
+++ Spring-review-IoC/src/main/java/stereotype_annotation/MyApp.java
@@ -1,16 +1,24 @@
 package stereotype_annotation;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import stereotype_annotation.config.Config;
 import stereotype_annotation.model.DataStructure;
+import stereotype_annotation.model.Microservices;
 
 public class MyApp {
 
     public static void main(String[] args) {
 
         ApplicationContext container = new  AnnotationConfigApplicationContext(Config.class);
+
         container.getBean(DataStructure.class).getHours();
+        container.getBean(Microservices.class).getHours();
+
+
+
+
+
+
     }
 }
