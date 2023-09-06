69474e07d6c9c6659d112e5a19fc880af37b3079
Abdellatif Zouliga <zuuliga@gmail.com>
2023-09-06 18:24:25 -0400

    Spring MVC Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,7 +1,12 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @org.springframework.stereotype.Controller
 public class StudentController {
@@ -13,6 +18,30 @@ public class StudentController {
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
 
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        // create some student id and show in UI
+
+        model.addAttribute("student_id_1", 203);
+        model.addAttribute("student_id_2", 99);
+        model.addAttribute("student_id_3", 3);
+        model.addAttribute("student_id_4", 26);
+
+        Random random = new Random();
+        model.addAttribute("myId",random.nextInt(1000));
+
+        List<Integer> numbers = new ArrayList<>(){{
+            add(random.nextInt(1000));
+            add(random.nextInt(1000));
+            add(random.nextInt(1000));
+            add(random.nextInt(1000));
+            add(random.nextInt(1000));
+        }};
+        model.addAttribute("numbers", numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
 
         return "student/welcome";
     }
