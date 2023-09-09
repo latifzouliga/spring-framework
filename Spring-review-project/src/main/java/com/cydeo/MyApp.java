61881c5102211e202a07287a3772c233d3b4af59
Abdellatif Zouliga <zuuliga@gmail.com>
2023-09-09 10:47:15 -0400

    task 4

---
+++ Spring-review-project/src/main/java/com/cydeo/MyApp.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+
+import com.cydeo.config.EmployeeConfig;
+import com.cydeo.service.SalaryService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
+
+        SalaryService service = context.getBean(SalaryService.class);
+        service.calculateRegularSalary();
+
+    }
+}
