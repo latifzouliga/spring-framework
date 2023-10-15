50b7668dd2a3365ecfc8b5b81cf9b1f2478e72b3
Abdellatif Zouliga <zuuliga@gmail.com>
2023-10-15 17:44:46 -0400

    added entity POJOs

---
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO EMPLOYEE(id, firstName, lastName)
+-- VALUES (1, 'Mike Smith');
+
+
+INSERT INTO Student(id,first_name, last_name)
+VALUES (1,'Mike', 'Smith');
+INSERT INTO Student(id,first_name, last_name)
+VALUES (2,'Latif', 'Zouliga');
+INSERT INTO Student(id,first_name, last_name)
+VALUES (3,'Kamal', 'Nori');
---
+++ Spring-10-ORM-TableCreation/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+-- create table EMPLOYEE(
+--              id bigint not null,
+--              first_name varchar(255),
+--              last_name varchar(255),
+--              primary key (id)
+-- );
+
+create table Student(
+    id bigint not null,
+    first_name varchar(255) not null ,
+    last_name  varchar(255) not null ,
+    primary key (id)
+);
