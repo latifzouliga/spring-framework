f8f27f7a786f15e8f22d0fd904ba445b0c48791c
Abdellatif Zouliga <zuuliga@gmail.com>
2023-12-07 12:44:50 -0500

    added OpenApi3 module

---
+++ Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/CinemaController.java
@@ -0,0 +1,32 @@
+package com.cydeo.controller;
+
+import com.cydeo.entity.Cinema;
+import com.cydeo.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class CinemaController {
+
+    private final CinemaRepository cinemaRepository;
+
+    public CinemaController(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+    @GetMapping("/cinemas")
+    public List<Cinema> readAllCinemas(){
+        return cinemaRepository.findAll();
+    }
+
+}
---
+++ Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.cydeo.controller;
+
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> readAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
